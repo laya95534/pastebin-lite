a9cff554a3256bac4a3fcb736692a9c8650b3a6e
Laya Gavi <layagavi@Layas-MacBook-Pro.local>
2026-01-27 20:30:52 +0530

    Convert to Vercel serverless

--- server.js
+++ server.js
@@ -43,7 +43,5 @@ app.get("/pastes/:id", (req, res) => {
   res.send(paste);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+module.exports = app;
+
