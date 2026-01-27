6115a8d67e720a4fd8270f508a0b2aa1be56ed40
Laya Gavi <layagavi@Layas-MacBook-Pro.local>
2026-01-27 16:55:17 +0530

    Add home route

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get("/pastes/:id", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
 
