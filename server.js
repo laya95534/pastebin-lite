ba01b5e9efe6adb4aeb241a2c8f16c965792de6c
Laya Gavi <layagavi@Layas-MacBook-Pro.local>
2026-01-27 15:18:30 +0530

    Fix port for Render deployment

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ app.get("/pastes/:id", (req, res) => {
 
   res.send(paste);
 });
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
