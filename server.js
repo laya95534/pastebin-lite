0a5e99dcbae28d2555d2fc9db849f15c1fea57b6
Laya Gavi <layagavi@Layas-MacBook-Pro.local>
2026-01-27 14:51:51 +0530

    Pastebin Lite project

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const { nanoid } = require("nanoid");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pastes = {};
+
+app.get("/", (req, res) => {
+  res.send("Pastebin Lite API is running 🚀");
+});
+
+app.post("/pastes", (req, res) => {
+  const { text } = req.body;
+
+  if (!text) {
+    return res.status(400).json({ error: "Text is required" });
+  }
+
+  const id = nanoid(6);
+  pastes[id] = text;
+
+  res.json({ id, url: `http://localhost:3000/pastes/${id}` });
+});
+
+app.get("/pastes/:id", (req, res) => {
+  const paste = pastes[req.params.id];
+
+  if (!paste) {
+    return res.status(404).json({ error: "Paste not found" });
+  }
+
+  res.send(paste);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
